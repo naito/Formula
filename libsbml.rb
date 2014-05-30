30842b3e057eddf7c145922c5800f5dc5aad2e82
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2014-05-30 22:42:47 +0900

    libsbml: test cmake

--- libsbml.rb
+++ libsbml.rb
@@ -5,9 +5,10 @@ class Libsbml < Formula
   url 'http://sourceforge.net/projects/sbml/files/libsbml/5.10.0/stable/libSBML-5.10.0-core-src.tar.gz'
   sha1 'a428d0992d384dd0d023644b04072e7f922c33c9'
 
-  depends_on :autoconf
-  depends_on :automake
-  depends_on :libtool
+#  depends_on :autoconf
+#  depends_on :automake
+#  depends_on :libtool
+  depends_on "cmake" => :build
   depends_on 'libxml2'
   depends_on :python => ["2.7", :recommended]
   depends_on 'swig'
@@ -15,12 +16,19 @@ class Libsbml < Formula
   def install
 
     ENV['ARCHFLAGS'] = "ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future"
-    system "./configure", "--prefix=#{prefix}",
-                          "--with-libxml",
-                          "--with-swig",
-                          "--with-java",
-                          "--with-python"
-    system "make", "install"
+#    system "./configure", "--prefix=#{prefix}",
+#                          "--with-libxml",
+#                          "--with-swig",
+#                          "--with-java",
+#                          "--with-python"
+#    system "make", "install"
+
+    system "cmake", ".", "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+                         "-DWITH_LIBXML=ON"
+                         "-DWITH_SWIG=ON"
+                         "-DWITH_JAVA=ON"
+                         "-DWITH_PYTHON=ON"
+
   end
 
   test do
