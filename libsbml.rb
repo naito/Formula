9d86b6cb15d791c92baee6fc8101759c3c2c92db
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2014-05-30 23:04:39 +0900

    libsbml: GNU make, python prefix

--- libsbml.rb
+++ libsbml.rb
@@ -5,10 +5,10 @@ class Libsbml < Formula
   url 'http://sourceforge.net/projects/sbml/files/libsbml/5.10.0/stable/libSBML-5.10.0-core-src.tar.gz'
   sha1 'a428d0992d384dd0d023644b04072e7f922c33c9'
 
-#  depends_on :autoconf
-#  depends_on :automake
-#  depends_on :libtool
-  depends_on "cmake" => :build
+  depends_on :autoconf
+  depends_on :automake
+  depends_on :libtool
+#  depends_on "cmake" => :build
   depends_on 'expat'
   depends_on :python => ["2.7", :recommended]
   depends_on 'swig'
@@ -16,20 +16,20 @@ class Libsbml < Formula
   def install
 
     ENV['ARCHFLAGS'] = "ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future"
-#    system "./configure", "--prefix=#{prefix}",
-#                          "--with-libxml",
-#                          "--with-swig",
-#                          "--with-java",
-#                          "--with-python"
-#    system "make", "install"
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-libxml",
+                          "--with-swig",
+                          "--with-java",
+                          "--with-python=/usr"
+    system "make", "install"
 
-    system "mkdir", "./build"
-    system "cd", "build/"
-    system "cmake", "..", "-DCMAKE_INSTALL_PREFIX=#{prefix}",
-                          "-DWITH_EXPAT=ON",
-                          "-DWITH_SWIG=ON",
-                          "-DWITH_JAVA=ON",
-                          "-DWITH_PYTHON=ON"
+#    system "mkdir", "./build"
+#    system "cd", "build/"
+#    system "cmake", "..", "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+#                          "-DWITH_EXPAT=ON",
+#                          "-DWITH_SWIG=ON",
+#                          "-DWITH_JAVA=ON",
+#                          "-DWITH_PYTHON=ON"
 
   end
 
