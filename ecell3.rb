1550b3b79ba9ad1720402f50a1f5bfb190c9c2b9
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2014-08-06 11:33:06 +0900

    empy 3.3.2

--- ecell3.rb
+++ ecell3.rb
@@ -2,10 +2,14 @@ require 'formula'
 
 class Ecell3 < Formula
   homepage 'https://github.com/ecell/ecell3'
-  url 'https://github.com/ecell/ecell3.git', :using => :git
-  sha1 'c7bc6a690e59725176bf3b5d1a762ad44a81b866'
+#  url 'https://github.com/ecell/ecell3.git', :using => :git
+#  sha1 'c7bc6a690e59725176bf3b5d1a762ad44a81b866'
 #  url 'https://github.com/ecell/ecell3/archive/naito-SBML2EML.tar.gz'
 #  sha1 '6d4b5f57ed692386a1969dbf8dd5d50a493fd456'
+#  url 'https://github.com/ecell/ecell3/archive/naito-StepperDebug.tar.gz'
+#  sha1 'f96255e61b9d21991a9429240bec6bcdcd58d76f'
+  url 'https://github.com/ecell/ecell3/archive/empy3.3.2.tar.gz'
+  sha1 'f96255e61b9d21991a9429240bec6bcdcd58d76f'
 
   depends_on :x11
   depends_on :autoconf
