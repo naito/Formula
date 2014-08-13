332704d0964a026a9cf2d1cc15f298ffdce637b8
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2014-08-13 23:22:45 +0900

    fix sha1

--- ecell3.rb
+++ ecell3.rb
@@ -7,7 +7,7 @@ class Ecell3 < Formula
 #  url 'https://github.com/ecell/ecell3/archive/naito-SBML2EML.tar.gz'
 #  sha1 '6d4b5f57ed692386a1969dbf8dd5d50a493fd456'
   url 'https://github.com/ecell/ecell3/archive/naito-ipython.tar.gz'
-  sha1 '45c0259329a180fb0ac913122fc92d912f61527e'
+  sha1 'd6a91d9e79fb83b7cd6a8026ff5b60f2926383b3'
 
   depends_on :x11
   depends_on :autoconf
