af55ebda603325653adb6ca1309607cde8fad6ae
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2016-11-26 14:00:22 +0900

    add autogen etc

--- torque.rb
+++ torque.rb
@@ -8,6 +8,9 @@ class Torque < Formula
   url 'https://github.com/adaptivecomputing/torque.git', :using => :git
   sha256 '2a76396253755e4a71a42f0eb7a7d4b96128f8360f4db19029ee10eeb26efa0b'
 
+  depends_on "autoconf"
+  depends_on "automake"
+  depends_on "libtool"
   depends_on 'libxml2'
   depends_on 'openssl'
   depends_on 'homebrew/dupes/tcl-tk'
