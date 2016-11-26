caa9e78fd40d6825a7779a052d9a0952bff662de
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2016-11-26 13:39:29 +0900

    use git

--- torque.rb
+++ torque.rb
@@ -3,9 +3,10 @@ require 'formula'
 class Torque < Formula
   version "6.1.0"
   homepage 'http://www.adaptivecomputing.com/products/open-source/torque/'
-  url 'http://www.adaptivecomputing.com/download/torque/torque-6.1.0.tar.gz'
+#  url 'http://www.adaptivecomputing.com/download/torque/torque-6.1.0.tar.gz'
+#  sha256 '2a76396253755e4a71a42f0eb7a7d4b96128f8360f4db19029ee10eeb26efa0b'
+  url 'https://github.com/adaptivecomputing/torque.git', :using => :git
   sha256 '2a76396253755e4a71a42f0eb7a7d4b96128f8360f4db19029ee10eeb26efa0b'
-#  url 'https://github.com/adaptivecomputing/torque.git', :using => :git
 
   depends_on 'libxml2'
   depends_on 'openssl'
