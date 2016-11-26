768dc5512457c7cef66e6b18cca73205d7fd2b6b
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2016-11-26 13:34:21 +0900

    sha256

--- torque.rb
+++ torque.rb
@@ -4,7 +4,7 @@ class Torque62 < Formula
   version "6.1.0"
   homepage 'http://www.adaptivecomputing.com/products/open-source/torque/'
   url 'http://www.adaptivecomputing.com/download/torque/torque-6.1.0.tar.gz'
-  sha1 '6edd7c6e362cc6f66ceba5be0d8440a124c2d23e'
+  sha256 '6edd7c6e362cc6f66ceba5be0d8440a124c2d23e'
 #  url 'https://github.com/adaptivecomputing/torque.git', :using => :git
 
   depends_on :libxml2
