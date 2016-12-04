b6c36ec7d6b92f8e04be87a123c6f7e601f8391d
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2016-12-04 16:44:57 +0900

    torque: source url, configure libxml2

--- torque.rb
+++ torque.rb
@@ -5,7 +5,8 @@ class Torque < Formula
   homepage 'http://www.adaptivecomputing.com/products/open-source/torque/'
 #  url 'http://www.adaptivecomputing.com/download/torque/torque-6.1.0.tar.gz'
 #  sha256 '74d857e22f1082ed2fb2ba7976c129b73791177f504e10ce10362a6e8a9e1c8a'
-  url 'https://github.com/adaptivecomputing/torque.git', :using => :git
+#  url 'https://github.com/adaptivecomputing/torque.git', :using => :git
+  url 'https://github.com/adaptivecomputing/torque/archive/v5.1.1.tar.gz'
   sha256 '2a76396253755e4a71a42f0eb7a7d4b96128f8360f4db19029ee10eeb26efa0b'
 
   depends_on "autoconf"
@@ -20,8 +21,8 @@ class Torque < Formula
    inreplace "autogen.sh", "libtoolize", "glibtoolize"
    system "sh", "./autogen.sh"
 
+   inreplace "configure", "sed 's/-L@<:@^@<:@:space:@:>@@:>@* //g;s/-l//'", "sed -E 's/-L@<:@^@<:@:space:@:>@@:>@* //g;s/-l(@<:@^@<:@:space:@:>@@:>@+).*/\1/g'"
     system "./configure", "--enable-cgroups",
-                          "--with-xml2-include=#{prefix}/include/libxml2",
                           "--with-hwloc-path=#{prefix}"
     system "make", "install"
   end
