b2763c296ae1f3bf6d4941237c17131765c8805d
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2016-12-04 14:09:38 +0900

    better solution for glibtoolize

--- torque.rb
+++ torque.rb
@@ -3,10 +3,10 @@ require 'formula'
 class Torque < Formula
   version "6.1.0"
   homepage 'http://www.adaptivecomputing.com/products/open-source/torque/'
-#  url 'http://www.adaptivecomputing.com/download/torque/torque-6.1.0.tar.gz'
-#  sha256 '2a76396253755e4a71a42f0eb7a7d4b96128f8360f4db19029ee10eeb26efa0b'
-  url 'https://github.com/naito/torque.git', :using => :git
+  url 'http://www.adaptivecomputing.com/download/torque/torque-6.1.0.tar.gz'
   sha256 '2a76396253755e4a71a42f0eb7a7d4b96128f8360f4db19029ee10eeb26efa0b'
+#  url 'https://github.com/naito/torque.git', :using => :git
+#  sha256 '2a76396253755e4a71a42f0eb7a7d4b96128f8360f4db19029ee10eeb26efa0b'
 
   depends_on "autoconf"
   depends_on "automake"
@@ -17,7 +17,11 @@ class Torque < Formula
   depends_on 'boost'
 
   def install
-    system "sh", "./autogen.sh"
+    if build.head?
+      inreplace "autogen.sh", "libtoolize", "glibtoolize"
+      system "./autogen.sh"
+    end
+
     system "./configure", "--enable-cgroups",
                           "--with-xml2-include=#{prefix}/include/libxml2",
                           "--with-hwloc-path=#{prefix}"
