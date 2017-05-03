4a32b79483f3818b7272562504d9fcdc93ca0bc8
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2017-05-03 21:40:59 +0900

    correct sha256

--- ecell3.rb
+++ ecell3.rb
@@ -5,7 +5,7 @@ class Ecell3 < Formula
   homepage 'https://github.com/naito/ecell3'
 #  url 'https://github.com/naito/ecell3.git', :using => :git
   url 'https://github.com/naito/ecell3/archive/3.2.4.tar.gz'
-  sha256 'ac1a5564c314a2eec5c0ef7cc6ba248e79e1f0ac'
+  sha256 '0db438ae4938598a4fc6502d9373a499f313723152e68d8d986ed3f7855e9b22'
 
   depends_on :x11
   depends_on "autoconf"
