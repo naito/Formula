ed11b85d8375e80fff806cc82bda82961e1a3721
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2017-05-03 21:37:30 +0900

    E-Cell3 3.2.4

--- ecell3.rb
+++ ecell3.rb
@@ -1,12 +1,11 @@
 require 'formula'
 
 class Ecell3 < Formula
-  version "3.2.3.160907"
+  version "3.2.4.170503"
   homepage 'https://github.com/naito/ecell3'
-  url 'https://github.com/naito/ecell3.git', :using => :git
+#  url 'https://github.com/naito/ecell3.git', :using => :git
+  url 'https://github.com/naito/ecell3/archive/3.2.4.tar.gz
   sha256 'ac1a5564c314a2eec5c0ef7cc6ba248e79e1f0ac'
-#  url 'https://github.com/ecell/ecell3/archive/naito-SBML2EML.tar.gz'
-#  sha256 '6d4b5f57ed692386a1969dbf8dd5d50a493fd456'
 
   depends_on :x11
   depends_on "autoconf"
