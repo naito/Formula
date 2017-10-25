e07a5f4826781ece6b6365ae0eaa51e99d329e5e
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2017-10-25 22:07:20 +0900

    Update libsbml.rb

--- libsbml.rb
+++ libsbml.rb
@@ -2,17 +2,13 @@ require 'formula'
 
 class Libsbml < Formula
   homepage 'http://sbml.org/Software/libSBML'
-#  url 'http://sourceforge.net/projects/sbml/files/libsbml/5.9.0/stable/libSBML-5.9.0-core-src.tar.gz'
-#  sha256 'e2b42dfc45a9a1a88da73253ff3655ce5e25a6f1'
-  url 'http://sourceforge.net/projects/sbml/files/libsbml/5.10.0/stable/libSBML-5.10.0-core-src.tar.gz'
-  sha256 'c7e733443caaf4615e90a29111ff988b89b1c9a679de0a9737fc2a86840df1e0'
+  url 'http://sourceforge.net/projects/sbml/files/libsbml/5.15.0/stable/libSBML-5.15.0-core-src.tar.gz'
+  sha256 'f975bd1fe379cc7027fd3fed9d6ae92e6dd61dda09befdfd2ffb969c324a5c28'
 
   depends_on "autoconf"
   depends_on "automake"
   depends_on "libtool"
-#  depends_on "cmake" => :build
   depends_on 'libxml2'
-#  depends_on 'expat'
   depends_on :python => ["2.7", :recommended]
   depends_on 'swig'
 
@@ -21,20 +17,9 @@ class Libsbml < Formula
     ENV['ARCHFLAGS'] = "ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future"
     system "./configure", "--prefix=#{prefix}",
                           "--with-libxml",
-#                          "--with-expat",
                           "--with-swig",
-#                          "--with-java",
-#                          "--with-python", "--with-python-interpreter=/usr/bin/python2.7"
                           "--without-python"
     system "make", "install"
-
-#    system "mkdir", "./build"
-#    system "cd", "build/"
-#    system "cmake", "..", "-DCMAKE_INSTALL_PREFIX=#{prefix}",
-#                          "-DWITH_EXPAT=ON",
-#                          "-DWITH_SWIG=ON",
-#                          "-DWITH_JAVA=ON",
-#                          "-DWITH_PYTHON=ON"
 
   end
 
