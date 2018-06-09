da5d99cc2f39ff336e917f2c81c892c08f3430e7
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2018-06-09 16:09:26 +0900

    Revise devel

--- ecell3.rb
+++ ecell3.rb
@@ -6,10 +6,10 @@ class Ecell3 < Formula
   url 'https://github.com/naito/ecell3/releases/download/ecell-3.2.7/ecell-3.2.7.tar.gz'
   sha256 '7f442b644b77b3732c315993256bcc33257602c41076b85673a39f420cfcc290'
 
-_devel = false
+$devel = false
 
   devel do
-_devel = true
+$devel = true
     url "https://github.com/naito/ecell3/archive/session-monitor-2018.tar.gz"
     sha256 "e34fce73e53029d1ce7f51b4e4e0554402d5a902761731bb22783618c681650f"
   end
@@ -28,7 +28,7 @@ _devel = true
 
   def install
 
-    if _devel == true then
+    if $devel == true then
       system "./autogen.sh"
     end
 
