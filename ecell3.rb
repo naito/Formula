90f23e2b98e52b93ed016d380fc314745384590d
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2018-06-09 15:57:32 +0900

    Add devel

--- ecell3.rb
+++ ecell3.rb
@@ -6,6 +6,11 @@ class Ecell3 < Formula
   url 'https://github.com/naito/ecell3/releases/download/ecell-3.2.7/ecell-3.2.7.tar.gz'
   sha256 '7f442b644b77b3732c315993256bcc33257602c41076b85673a39f420cfcc290'
 
+  devel do
+    url "https://github.com/naito/ecell3/archive/session-monitor-2018.tar.gz"
+    sha256 "e34fce73e53029d1ce7f51b4e4e0554402d5a902761731bb22783618c681650f"
+  end
+
   depends_on :x11
 #  depends_on "python@2" if MacOS.version <= :snow_leopard
   depends_on 'python'
@@ -19,6 +24,11 @@ class Ecell3 < Formula
   depends_on 'readline' => :recommended
 
   def install
+
+    devel do
+      system "./autogen.sh"
+    end
+
     system "./configure", "--with-boost-python-libname=boost_python27-mt", 
                           "--disable-debug",
                           "--disable-dependency-tracking",
