927c475b8ffc92d164077fff547551d0987e5666
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2018-06-10 12:14:30 +0900

    Revise glade_gtk2.rb

--- glade_gtk2.rb
+++ glade_gtk2.rb
@@ -1,4 +1,4 @@
-class Glade_GTK2 < Formula
+class GladeGTK2 < Formula
   desc "RAD tool for the GTK+ and GNOME environment"
   homepage "https://glade.gnome.org/"
   url "https://download.gnome.org/sources/glade/3.8/glade-3.8.6.tar.xz"
