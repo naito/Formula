b0ac71639618ddc063701f80162679598dbcce9e
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2018-10-06 21:40:39 +0900

    Update ecell3.rb

--- ecell3.rb
+++ ecell3.rb
@@ -56,8 +56,8 @@ $devel = true
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
-                          "--libdir=#{HOMEBREW_PREFIX}/lib",
-                          "--includedir=#{HOMEBREW_PREFIX}/include"
+                          "LDFLAGS -L#{HOMEBREW_PREFIX}/lib",
+                          "CPPFLAGS -I#{HOMEBREW_PREFIX}/include"
     system "make", "install"
   end
 
