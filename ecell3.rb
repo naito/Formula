e89670c4e59b361601851e491061710ed934c567
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2019-05-17 15:09:58 +0900

    Update ecell3.rb

--- ecell3.rb
+++ ecell3.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Ecell3 < Formula
   version "3.2.8"
   homepage 'https://github.com/naito/ecell3'
-  url 'https://github.com/naito/ecell3/releases/download/ecell-3.2.8/ecell-3.2.8.tar.gz'
-  sha256 '32612fd2cb8c81f38e97a0e74985efc405c2cefa39b16413793dcdfc21c46c87'
-  revision 1
+  url 'https://github.com/naito/ecell3/tarball/30499e5079a94557f96282179f9572ca7019875a'
+  sha256 '8c87f204c7f63b34780f0db676c523fe9d84f96efbc47604d9ce1858eaa058b2'
+  revision 2
 
 $devel = false
 
