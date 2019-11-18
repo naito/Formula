b1e72f6a3cd82ab5d1c4eb3e5a107dc36dc5b404
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2019-11-18 15:49:33 +0900

    Update ecell33.rb

--- ecell33.rb
+++ ecell33.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
-class Ecell3 < Formula
+class Ecell33 < Formula
   version "3.3-alpha.1"
   homepage 'https://github.com/naito/ecell3'
   url 'https://github.com/naito/ecell3/archive/ecell-3.3-alpha.1.tar.gz'
+  sha256 '257ad2350f3b9478443e325c72d24586349ac56c04d82fb03b327e3a2df3127f'
   revision 1
 
   depends_on :x11
@@ -20,7 +21,7 @@ class Ecell3 < Formula
   def install
 
     system "./autogen.sh"
-    
+
     system "./configure", "--with-boost-python-libname=boost_python37-mt",
                           "--disable-debug",
                           "--disable-dependency-tracking",
